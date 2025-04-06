7965965a858e75cf2304d53c147b13e36abe35ce
zhfrann <muhzhafranilham13@gmail.com>
2025-04-06 20:53:58 +0700

    menambahkan class DataMahasiswa103022330008

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello, World!");
+        DataMahasiswa103022330008.ReadJSON();
     }
 }
