4573932cab5673b34094bf14f308439268f690d7
zhfrann <muhzhafranilham13@gmail.com>
2025-04-06 21:10:17 +0700

    menambahkan class KuliahMahasiswa103022330008

---
+++ KuliahMahasiswa103022330008.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+class KuliahMahasiswa103022330008
+{
+    public List<Course> courses { get; set; }
+
+    public static void ReadJSON()
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tp7_2_103022330008.json");
+        string jsonString = File.ReadAllText(filePath);
+
+        KuliahMahasiswa103022330008 kuliahData = JsonSerializer.Deserialize<KuliahMahasiswa103022330008>(jsonString);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        for (int i = 0; i < kuliahData.courses.Count; i++)
+        {
+            Console.WriteLine($"MK {i+1} {kuliahData.courses[i].code} - {kuliahData.courses[i].name}");
+        }
+    }
+}
+
+class Course
+{
+    public string code { get; set; }
+    public string name { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -4,5 +4,7 @@
     {
         //Console.WriteLine("Hello, World!");
         DataMahasiswa103022330008.ReadJSON();
+
+        KuliahMahasiswa103022330008.ReadJSON();
     }
 }
