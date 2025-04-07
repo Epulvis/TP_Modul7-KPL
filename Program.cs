4d0e33e874ce7ab3dc97ad7e0f0ab0c816877116
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-04-07 17:58:33 +0700

    selesai

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
 ﻿using tpmodul7_kelompok_3;
 
 internal class Program
-<<<<<<< HEAD
-{
-    private static void Main(string[] args)
-    {
-=======
 
 {
     private static void Main(string[] args)
-    {
-        // Console.WriteLine("Hello, World!");
-        
->>>>>>> a5b31331666e7e8c7956a2b40d7e2e6b7479425b
+    {        
         DataMahasiswa103022300093.ReadJSON();
         KuliahMahasiswa103022300093.ReadJSON();
     }
